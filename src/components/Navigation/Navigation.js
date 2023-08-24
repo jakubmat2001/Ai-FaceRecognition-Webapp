534e39574415062b715396a64e6d14909067a4d1
jakubmat2001 <jakubmatusik11@gmail.com>
2023-08-24 22:27:22 +0100

    moved the logo component into nav component for merger

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../Logo/Logo";
 import './Navigation.css';
 
 
@@ -6,19 +7,25 @@ const Navigation = ({ onRouteChange, isSigned }) => {
     if (isSigned) {
         // If user is signed in, show signout buttons and send end-user to Signin route once clicked on
         return (
-        <nav className="navigation">
-            <p onClick={() => onRouteChange("signout")} className="navigation-buttons">Sing Out</p>
-            {console.log("IsSigned is true: " + isSigned)}
-        </nav>
+            <nav className="navigation-signin">
+                <div className="logo-container">
+                    <Logo />
+                </div>
+                <div className="buttons-container">
+                    <p onClick={() => onRouteChange("signout")} className="navigation-buttons">Sing Out</p>
+                </div>
+
+                {console.log("IsSigned is true: " + isSigned)}
+            </nav>
         );
     } else {
         // If not user not signed in, show Signin/register buttons and send user to one these routes once clicked on
         return (
-        <nav className="navigation">
-            <p onClick={() => onRouteChange("signin")} className="navigation-buttons">Sing In</p>
-            <p onClick={() => onRouteChange("register")} className="navigation-buttons">Register</p>
-            {console.log("IsSigned is false: " + isSigned)}
-        </nav>
+            <nav className="navigation">
+                <p onClick={() => onRouteChange("signin")} className="navigation-buttons">Sing In</p>
+                <p onClick={() => onRouteChange("register")} className="navigation-buttons">Register</p>
+                {console.log("IsSigned is false: " + isSigned)}
+            </nav>
         );
     }
 }
