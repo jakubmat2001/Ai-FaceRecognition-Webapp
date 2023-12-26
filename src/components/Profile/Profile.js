b83825ef645bc811537e575b7ad4172badb8a184
jakubmat2001 <jakubmatusik11@gmail.com>
2023-12-26 17:58:16 +0000

    changed logo, made changes edit profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -57,7 +57,7 @@ class Profile extends React.Component {
                         this.props.toggleModal();
                         window.location.reload();
                     }
-                    
+
                 }
             })
             .catch(error => {
@@ -80,38 +80,47 @@ class Profile extends React.Component {
         if (profileImg === null || profileImg === undefined) {
             profileImg = this.props.defaultProfileImg;
         }
+
         return (
             <div className="profile-container" >
                 <article className="profile-article-html">
                     <main className="profile-main-html">
                         <form className="profile-form-contents">
-                            <fieldset id="profile" className="profile-form-grouping">
-                                <div className="top-profile-container">
-                                    <img className="profile-img-modal" src={this.checkProfileImg(profileImg)} alt=""></img>
-                                    <legend className="profile-label">Profile</legend>
-                                    <div className="profile-exit" onClick={this.props.toggleModal}>X</div>
+                            <div className="top-profile-container">
+                                <img className="profile-img-modal" src={this.checkProfileImg(profileImg)} alt=""></img>
+                                <legend className="profile-label">Profile</legend>
+                                <div className="profile-exit" onClick={this.props.toggleModal}>X</div>
+                            </div>
+
+                            <hr />
+                            <p >{this.state.name}</p>
+                            <p >Images submitted: {this.props.user.entries}</p>
+                            <hr id="second-hr" />
+                            <section className="profile-input-updates-section">
+
+                                <div className="profile-input-update-container name">
+                                    <label className="profile-input-label" for="profile-name-input-box">Profile Name</label>
+                                    <input type="text" className="profile-name-input-box" name="name" onChange={this.onFormChange}></input>
                                 </div>
-                                <hr />
-                                <p className="profile-name-display">{this.state.name}</p>
-                                <p className="profile-image-display">Images submitted: {this.props.user.entries}</p>
-                                <hr className="second-horizontal-row" />
-                                <div className="profile-form-div">
-                                    <label className="profile-form-label" htmlFor="name">Name</label>
-                                    <input onChange={this.onFormChange} className="profile-input-properties" type="text" name="name"
-                                        id="name" placeholder={this.props.user.name} />
-                                    <br />
-                                    <label className="profile-form-label" htmlFor="profile-img">Profile Image</label>
-                                    <input onChange={this.onFormChange} className="profile-input-properties-img" type="file" name="profile-img"
-                                        id="profile-img" />
+
+                                <div className="profile-input-update-container image">
+                                    <label className="profile-input-label">Profile Image</label>
+                                    <label for="file" className="profile-file-upload">
+                                        <div className="icon">
+                                            <svg viewBox="0 0 24 24" fill="" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" fill=""></path> </g></svg>
+                                        </div>
+                                        <div className="text">
+                                            <span id="displayed-text">Click to upload image</span>
+                                        </div>
+                                        <input id="file" type="file" name="profile-img" onChange={this.onFormChange}></input>
+                                    </label>
                                 </div>
-                            </fieldset>
+                            </section>
                         </form>
                     </main>
                     <div className="modify-options-container">
-                        <div>
-                            <button className="save-changes" onClick={() => this.onProfileUpdate({ name, profileImg })}>Save Changes</button>
-                            <button className="cancel-changes" onClick={this.props.toggleModal}>Cancel</button>
-                        </div>
+                            <button className="profile-update-button" onClick={() => this.onProfileUpdate({ name, profileImg })}>Save Changes</button>
+                            <button className="profile-update-button" onClick={this.props.toggleModal}>Cancel</button>
                     </div>
                 </article>
             </div>
